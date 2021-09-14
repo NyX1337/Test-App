16699b3725f1c848ccae3cd2518975cd3c85a228
NyX1337 <bdnov123@hotmail.com>
2021-09-14 18:06:45 +0200

    first commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,104 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Text, View, StyleSheet } from 'react-native'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
+
+const Stack = createNativeStackNavigator()
+const Tab = createMaterialBottomTabNavigator()
 
 export default function App() {
   return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName='Home'
+        activeColor='yellow'
+        inactiveColor='white'
+        barStyle={{ backgroundColor: 'blue' }}
+      >
+        <Tab.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name='home' color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Search'
+          component={SearchScreen}
+          options={{
+            tabBarLabel: 'Search',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name='search' color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Notifications'
+          component={NotificationScreen}
+          options={{
+            tabBarLabel: 'Notifications',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name='notifications' color={color} size={26} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Settings'
+          component={SettingsScreen}
+          options={{
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name='settings' color={color} size={26} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+function SettingsScreen() {
+  return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Settings!</Text>
     </View>
-  );
+  )
+}
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Home!</Text>
+    </View>
+  )
+}
+
+function NotificationScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Notifications!</Text>
+    </View>
+  )
+}
+
+function SearchScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Search!</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightgrey',
   },
-});
+})
